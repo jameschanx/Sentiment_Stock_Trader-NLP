b468afe836c6fde97467230a0273f922cc9144dc
James Chan <jameschanx@gmail.com>
2018-07-29 16:08:34 -0700

    get data frame and daily return

--- st_utils.py
+++ st_utils.py
@@ -1,3 +1,12 @@
+'''
+Author: James Chan
+Date: 7/29/18
+'''
 import pandas as pd
 
-df = pd.read_csv('QQQ.csv')
+def get_data(ticker, dates):
+    df = pd.DataFrame(index=dates)
+    df_tmp = pd.read_csv('QQQ.csv', index_col='Date',
+            parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+    df = df.join(df_tmp)
+    return df
