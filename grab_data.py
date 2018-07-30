30b0c986bd45eb66c33c8d1818603a611d001f33
James Chan <jameschanx@gmail.com>
2018-07-29 18:17:17 -0700

    data grab successful

--- grab_data.py
+++ grab_data.py
@@ -10,37 +10,55 @@ import requests
 import pandas as pd
 
 def news_request(company, start_date, end_date, news_source):
-    apikey = '23e7e5c41b6443f1bb556ec312275aff'
-    
-    #determine the total number of results in the request parameter
-    url = 'https://newsapi.org/v2/everything?q={}&from={}&to={}&domains={}&pageSize=0&apiKey={}'\
-            .format(company, start_date, end_date, news_source, apikey)
-    response = requests.get(url)
-    news = response.json() 
-    total_result = news['totalResults']
-    
-    #create a dataframe to hold article and time it was published
-    df = pd.DataFrame(columns=['Published', 'Body'])
-    print(df)
     '''
-    once we have determined the total of number of articles, we loop through all the pages 
-    to get everything since each page has a max allowable articles displayed
+    arguments:
+        company (list(strings))
+        start_date (string)
+        end_date (string)
+        news_source (list(strings))
+    
+    returns: 
+        pandas.DataFrame
+        
+    description:
+        1. create dataframe to hold published time, title, body, and company.
+        2. determined the total of number of articles in the date range by the publishers
+        3. we loop through all the pages to get all the articles.
+        4. store article data to dataframe and return it.
     '''
-#    page_size = 100 # 100 is max number of page displayed per page per API
-#    for page in range(1, total_result/page_size + 2):
-#        url = 'https://newsapi.org/v2/everything?q={}&from={}&to={}&domains={}&pageSize={}&page={}apiKey={}'\
-#                .format(company, start_date, end_date, news_source, page_size, page, apikey)
-#        response = requests.get(url)
-#        news = response.json()
-#        
+    
+    apikey = '23e7e5c41b6443f1bb556ec312275aff' #unique API key per registered user
+
+    #create dataframe so we can append data to it later
+    df = pd.DataFrame(columns=['Published', 'Title', 'Body','Company'])
+    articles_per_page = 100 # 100 is max number of page displayed per page per API
+    for company in companies:
+        #determine the total number of articles for the given company and date range
+        url = 'https://newsapi.org/v2/everything?q={}&from={}&to={}&domains={}&pageSize=0&apiKey={}'\
+                .format(company, start_date, end_date, news_source, apikey)
+        response = requests.get(url)
+        news = response.json() 
+        num_articles = news['totalResults']
+        
+        #loop through the pages to get all the articles
+        for page in range(1, int(num_articles/articles_per_page) + 2):
+            print(company, num_articles, page)
+            url = 'https://newsapi.org/v2/everything?q={}&from={}&to={}&domains={}&pageSize={}&page={}&apiKey={}'\
+                    .format(company, start_date, end_date, news_source, articles_per_page, page, apikey)
+            response = requests.get(url)
+            news = response.json()
+            for article in news['articles']:
+                published = article['publishedAt']
+                title = article['title']
+                body = article['description']
+                df = df.append({'Published': published, 'Title' : title, 'Body': body, 'Company': company}, ignore_index=True)
+    return df
+
 if __name__=="__main__":
-    companies = ['twitter','amd','tesla'] #these three stocks move largely base on news
+    companies = ['twitter','netflix','tesla'] #these three stocks move largely base on news
     news_source = 'wsj.com, bloomberg.com, cnbc.com'
     start_date = '2016-01-01'
     end_date = '2017-12-31'
-    #news_request(companies, start_date, end_date, news_source)
-    df = pd.DataFrame(columns=['Published', 'Body'])
-    published = '1234'
-    body = 'a35hw4bea'
-    df = df.append({'Published': published, 'Body': body}, ignore_index=True)
+    df = news_request(companies, start_date, end_date, news_source)
     print(df)
+    df.to_csv('news_dataset.csv')
